818e53b81091e533994af6e45cf23f696142464f
damianicely <damiancely@gmail.com>
2020-09-11 21:37:59 +0100

    back to normal

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -92,7 +92,7 @@
   
                   <div class="row no-gutters sortable-container sortable-container-gutter-fix">
                     @foreach ($photos as $photo)
-                      <a class="col-6 col-md-3 sortable-item p-2 {{ str_replace(' ', '', $photo->collection) }}" href="x-{{ $photo->reference }}.jpg" data-lightbox="image" data-title="#{{ $$photo->reference }} - {{ $photo->description }}">
+                      <a class="col-6 col-md-3 sortable-item p-2 {{ str_replace(' ', '', $photo->collection) }}" href="x-{{ $photo->reference }}.jpg" data-lightbox="image" data-title="{{ $photo->description }}">
                         <img class="w-100 rounded" src="t-{{ $photo->reference }}.jpg" alt=""/>
                       </a>                      
                     @endforeach
