674a64ebaee1fe4a679f6c9d39a6f77a47fd2b0a
damianicely <damiancely@gmail.com>
2020-09-12 06:12:45 +0100

    Altered title box

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -47,24 +47,28 @@
             </div>
           </div>
           <div class="container">
-            <div class="row vh-100 align-items-center justify-content-center justify-content-lg-start py-10">
-              <div class="col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 z-index-1">
-                <div class="p-4 p-sm-5 bg-glass border border-2x border-primary parallax overflow-hidden rounded" data-zanim-trigger="scroll" data-zanim-timeline='{"delay":0.2}' data-rellax-speed="3">
-                  <div class="overflow-hidden">
-                    <h4 class="fs-1 fs-sm-2"><span class="d-block overflow-hidden"><span class="d-inline-block" data-zanim-xs='{"delay":0.1}'>Sebastião Pernes</span></span><span class="d-block overflow-hidden"><span class="d-inline-block" data-zanim-xs='{"delay":0.1}'>Photographia</span></span>
+            <div class="row vh-100 align-items-center justify-content-center justify-content-lg-start py-0">
+              <div class="col-9 col-sm-10 col-md-8 col-lg-6 col-xl-4 z-index-1">
+                <div class="p-3 p-sm-4 bg-glass border border-2x border-primary parallax overflow-hidden rounded" data-zanim-trigger="scroll" data-zanim-timeline='{"delay":0.2}' data-rellax-speed="3">
+                  <div class="overflow-hidden float-center text-center">
+                    <h4 class="fs-1 fs-sm-2 mb-2 py-0">
+                      <span class="d-block overflow-hidden">
+                        <span class="d-inline-block" data-zanim-xs='{"delay":0.1}'>Sebastião Pernes</span>
+                      </span>
+                    </h4>
+                    <h4 class="fs-1">
+                          <img src="/img/logo/logo_egypt.png" width="150px">  
+                      <span class="d-block overflow-hidden">
+                        <span class="d-inline-block" data-zanim-xs='{"delay":0.1}'>PHOTOGRAFIA</span>
+                      </span>
                     </h4>
                   </div>
-                  <div class="row align-items-center">
-                    <div class="col-6 overflow-hidden">
-                      <div data-zanim-xs='{"delay":0.1}'>
-                        <a class="btn btn-dark btn-sm mt-4" href="#portfolio">Portfolio</a>
+                  <div class="row align-items-center mt-2">
+                    <div class="col-12 overflow-hidden">
+                      <div data-zanim-xs='{"delay":0.1}' class="float-right">
+                        <a class="btn btn-dark btn-sm" href="#portfolio">Portfolio</a>
                       </div>
                     </div>  
-                    <div class="col-6 overflow-hidden">
-                      <div class="mt-4" data-zanim-xs='{"delay":0.1}'>
-                        <img src="/img/logo/logo_egypt.png" width="150px">
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
